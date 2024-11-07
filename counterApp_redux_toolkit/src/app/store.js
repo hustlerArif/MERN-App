20b0686a145c46e1097954ffdad4ff3e07f545e7
Md Arif <workwithmdarif@gmail.com>
2024-11-07 21:41:56 +0530

    completed counter app with redux toolkit

---
+++ counterApp_redux_toolkit/src/app/store.js
@@ -0,0 +1,14 @@
+// import { configureStore } from '@reduxjs/toolkit'
+
+// export const store = configureStore({
+//   reducer: {},
+// })
+
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+})
